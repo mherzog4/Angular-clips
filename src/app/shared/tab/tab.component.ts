1047ae39659f2b328995fe5650b2476f74e0b8c6
Mherzog <matthewherzog4@gmail.com>
2023-04-10 11:50:01 -0400

    component design

---
+++ src/app/shared/tab/tab.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css']
+})
+export class TabComponent {
+
+  @Input() tabTitle = ''
+  @Input() active = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
