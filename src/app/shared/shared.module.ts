6021c32d65e8553cf0f5354a9748fda19f9d017c
Mherzog <matthewherzog4@gmail.com>
2023-04-11 17:29:31 -0400

    reactive forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,31 @@
+import { NgxMaskModule } from '@NgxMaskModule';
+import { NgxMaskModule } from '@ngx-mask';
+import { NgxMaskModule } from 'ngx-mask';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-
-
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from '@Ngx-NgxMaskModule';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskModule.forRoot()],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
-  ]
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
